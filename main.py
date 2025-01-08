0e3ca7cef055e206c473b7d7df2e39d247748595
Kayla <kaylavd99@gmail.com>
2025-01-08 17:20:51 -0500

    Asteroids: set up pygame screen and functionality for closing it

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+
+def main():
+	pygame.init()
+	print("Starting asteroids!")
+	print("Screen width:", SCREEN_WIDTH)
+	print("Screen height:", SCREEN_HEIGHT)
+
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while (True):
+		screen.fill((0, 0, 0))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+	display.flip()
+if __name__ == "__main__":
+    main()
